58846364bf1a48609ad5311bfa09547690d30c9f
Parag Jadhav <>
2021-05-20 01:46:06 +0530

    added pagination and lazy loading

--- src/app/image-viewer/eg-image/eg-image.component.ts
+++ src/app/image-viewer/eg-image/eg-image.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Virtual, Zoom, Autoplay, Thumbs, Controller, EffectCoverflow } from 'swiper/core';
-import { SwiperComponent } from "swiper/angular";
+import SwiperCore, { Pagination, EffectCoverflow, Lazy } from 'swiper/core';
+import { SwiperModule, SwiperComponent } from 'swiper/angular';
 
-SwiperCore.use([ EffectCoverflow, Pagination ]);
+SwiperCore.use([EffectCoverflow, Pagination, Lazy]);
 @Component({
   selector: 'app-eg-image',
   templateUrl: './eg-image.component.html',
@@ -10,12 +10,48 @@ SwiperCore.use([ EffectCoverflow, Pagination ]);
   encapsulation: ViewEncapsulation.None,
 })
 export class EgImageComponent implements OnInit {
-
-  @ViewChild("swiperRef", { static: false }) swiperRef?: SwiperComponent;
+  @ViewChild('swiperRef', { static: false }) swiperRef?: SwiperModule;
 
   public thumbsSwiper: any;
+  public swiper: any;
+  public slides = [
+    "https://swiperjs.com/demos/images/nature-10.jpg",
+    "https://swiperjs.com/demos/images/nature-2.jpg",
+    "https://swiperjs.com/demos/images/nature-3.jpg",
+    "https://swiperjs.com/demos/images/nature-4.jpg",
+    "https://swiperjs.com/demos/images/nature-5.jpg",
+    "https://swiperjs.com/demos/images/nature-6.jpg",
+    "https://swiperjs.com/demos/images/nature-7.jpg",
+    "https://swiperjs.com/demos/images/nature-8.jpg",
+  ];
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.swiper = {
+      autoHeight: "true",
+      effect: "coverflow",
+      grabCursor: "true",
+      centeredSlides: "true",
+      slidesPerView: "auto",
+      coverflowEffect: { 
+        rotate: 50, 
+        stretch: 0, 
+        depth: 100, 
+        modifier: 1, 
+        slideShadows: true 
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+      },
+      preloadImages: false,
+      lazy: {
+        loadPrevNext: true,
+        elementClass: "swiper-lazy"
+      },
+    };
+  }
+
   setThumbsSwiper(swiper: any) {
     this.thumbsSwiper = swiper;
   }
