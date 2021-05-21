df8d2f49ad8f9888cbb9be3e3150ea2cc0c4b01b
Parag Jadhav <>
2021-05-22 03:09:05 +0530

    added background image as blur

--- src/app/image-viewer/eg-image/eg-image.component.ts
+++ src/app/image-viewer/eg-image/eg-image.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import SwiperCore, { Pagination, EffectCoverflow, Lazy } from 'swiper/core';
-import { SwiperModule, SwiperComponent } from 'swiper/angular';
+import { SwiperModule } from 'swiper/angular';
 
 SwiperCore.use([EffectCoverflow, Pagination, Lazy]);
 @Component({
@@ -15,41 +15,46 @@ export class EgImageComponent implements OnInit {
   public thumbsSwiper: any;
   public swiper: any;
   public slides = [
-    "https://swiperjs.com/demos/images/nature-10.jpg",
-    "https://swiperjs.com/demos/images/nature-2.jpg",
-    "https://swiperjs.com/demos/images/nature-3.jpg",
-    "https://swiperjs.com/demos/images/nature-4.jpg",
-    "https://swiperjs.com/demos/images/nature-5.jpg",
-    "https://swiperjs.com/demos/images/nature-6.jpg",
-    "https://swiperjs.com/demos/images/nature-7.jpg",
-    "https://swiperjs.com/demos/images/nature-8.jpg",
+    'https://swiperjs.com/demos/images/nature-10.jpg',
+    'https://swiperjs.com/demos/images/nature-2.jpg',
+    'https://swiperjs.com/demos/images/nature-3.jpg',
+    'https://swiperjs.com/demos/images/nature-4.jpg',
+    'https://swiperjs.com/demos/images/nature-5.jpg',
+    'https://swiperjs.com/demos/images/nature-6.jpg',
+    'https://swiperjs.com/demos/images/nature-7.jpg',
+    'https://swiperjs.com/demos/images/nature-8.jpg',
   ];
+  public activeImage: any = this.slides[0];
 
   ngOnInit() {
-
     this.swiper = {
-      autoHeight: "true",
-      effect: "coverflow",
-      grabCursor: "true",
-      centeredSlides: "true",
-      slidesPerView: "auto",
-      coverflowEffect: { 
-        rotate: 50, 
-        stretch: 0, 
-        depth: 100, 
-        modifier: 1, 
-        slideShadows: true 
+      autoHeight: 'true',
+      effect: 'coverflow',
+      grabCursor: 'true',
+      centeredSlides: 'true',
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
       },
       pagination: {
-        el: ".swiper-pagination",
-        type: "bullets",
+        el: '.swiper-pagination',
+        type: 'bullets',
       },
       preloadImages: false,
       lazy: {
         loadPrevNext: true,
-        elementClass: "swiper-lazy"
+        elementClass: 'swiper-lazy',
       },
     };
+  }
+
+  onSlideChange(event: any) {
+    this.activeImage = this.slides[event.realIndex];
+    console.log('slide change to index', this.activeImage);
   }
 
   setThumbsSwiper(swiper: any) {
